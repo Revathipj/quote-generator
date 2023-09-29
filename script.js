2875a02032ceac5661aeb2b63162e5027f5bbf22
Revathi P J <pjrevathi4@gmail.com>
2023-09-29 21:43:40 +0530

    Fetching Data with javascript

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-// let apiQuotes = [];
+let apiQuotes = [];
 
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   console.log(quote);
 }
 
-// // Get Quotes from API
-// async function getQuotes() {
-//   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     // Catch Error Here
-//   }
+// function newQuote() {
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+//   console.log(quote);
 // }
 
-// //On Load
-// getQuotes(); 
-newQuote();
+
+
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+//On Load
+getQuotes(); 
