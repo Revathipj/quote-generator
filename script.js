696c4646076e03a9b7c2ee1e3732f636afc2bfc5
Revathi P J <pjrevathi4@gmail.com>
2023-10-01 21:59:57 +0530

    update manipulate DOM with Javascript

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-let apiQuotes = [];
+// let apiQuotes = [];
 // Show Loading
 function loading() {
   loader.hidden = false;
@@ -18,26 +18,29 @@ function complete() {
 }
 
 // Show New Quote
-function newQuote() {
-  loading();
-  // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  //Check if Author field is blank and replace it with 'Unknown'
-  if (!quote.author) {
-    authorText.textContent = 'Unknown';
-  } else {
-    authorText.textContent = quote.author;
-  }
-  // Check Quote length to determine styling
-  if (quote.text.length > 120) {
-    quoteText.classList.add('long-quote');
-  } else {
-    quoteText.classList.remove('long-quote');
-  }
-  // Set Quote, Hide Loader
-  quoteText.textContent = quote.text;
-  complete();
-}
+// function newQuote() {
+//   loading();
+//   // Pick a random quote from apiQuotes array
+//   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+//   //Check if Author field is blank and replace it with 'Unknown'
+//   // if (!quote.author) {
+//   //   authorText.textContent = 'Unknown';
+//   // } else {
+//   //   authorText.textContent = quote.author;
+//   // }
+
+//   // Check Quote length to determine styling
+//   // if (quote.text.length > 120) {
+//   //   quoteText.classList.add('long-quote');
+//   // } else {
+//   //   quoteText.classList.remove('long-quote');
+//   // }
+
+//   // Set Quote, Hide Loader
+//   quoteText.textContent = quote.text;
+//   complete();
+// }
 
 // function newQuote() {
 //   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
@@ -50,13 +53,25 @@ function newQuote() {
 // Get Quotes from API
 async function getQuotes() {
   loading();
-
-  const proxyUrl
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
+    // apiQuotes = await response.json();
+    const data = await response.json();
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    // newQuote();
   } catch (error) {
     // Catch Error Here
     console.log('whoops, no quote', error);
@@ -64,13 +79,22 @@ async function getQuotes() {
 }
 
 // Tweet Quote
+// function tweetQuote() {
+//   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText / textContent}`;
+//   window.open(twitterUrl, '_blank')
+// }
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText / textContent}`;
-  window.open(twitterUrl, '_blank')
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl,'_blank');
 }
 
+
 // Event Listeners
-newQuoteBtn.addEventListener('click', newQuote);
+// newQuoteBtn.addEventListener('click', newQuote);
+// twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
