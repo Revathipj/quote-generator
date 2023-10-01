c3b5e0fc46baef0a932e5918d1dd64a0797705be
Revathi P J <pjrevathi4@gmail.com>
2023-10-01 14:54:42 +0530

    catch error update

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader=document.getElementById('loader');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 // Show Loading
-function loading(){
-  loader.hidden=false;
-  quoteContainer.hidden=true;
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 // Hide Loading
-function complete(){
-  quoteContainer.hidden=false;
-  loader.hidden=true;
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
 
 // Show New Quote
@@ -23,19 +23,19 @@ function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   //Check if Author field is blank and replace it with 'Unknown'
-  if (!quote.author){
-    authorText.textContent='Unknown';
-  }else{
-    authorText.textContent=quote.author;
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
   }
   // Check Quote length to determine styling
-  if(quote.text.length > 120){
+  if (quote.text.length > 120) {
     quoteText.classList.add('long-quote');
-  }else{
+  } else {
     quoteText.classList.remove('long-quote');
   }
   // Set Quote, Hide Loader
-  quoteText.textContent=quote.text;
+  quoteText.textContent = quote.text;
   complete();
 }
 
@@ -50,6 +50,8 @@ function newQuote() {
 // Get Quotes from API
 async function getQuotes() {
   loading();
+
+  const proxyUrl
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
@@ -57,18 +59,19 @@ async function getQuotes() {
     newQuote();
   } catch (error) {
     // Catch Error Here
+    console.log('whoops, no quote', error);
   }
 }
 
 // Tweet Quote
-function tweetQuote(){const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText/textContent}`;
-window.open(twitterUrl, '_blank')
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText / textContent}`;
+  window.open(twitterUrl, '_blank')
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click',newQuote);
-twitterBtn.addEventListener('click',tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuotes();
- 
